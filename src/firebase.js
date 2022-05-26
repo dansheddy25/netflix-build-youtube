fdc40e9b45fa311b69d793973df15f830987bc49
dansheddy25 <philipshedrack5@gmail.com>
2022-05-27 00:46:20 +0100

    my commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDCqFQwVaUQEZPhhuvoeYuz_Ng2_u5Q2TU",
+    authDomain: "netflix-clone-9d30f.firebaseapp.com",
+    projectId: "netflix-clone-9d30f",
+    storageBucket: "netflix-clone-9d30f.appspot.com",
+    messagingSenderId: "1018310559602",
+    appId: "1:1018310559602:web:9ac97ed87a09ad78857feb"
+  };
