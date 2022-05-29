d62fa09755ed2ab8d81adc39c90c03fa5da8e82e
dansheddy25 <philipshedrack5@gmail.com>
2022-05-29 07:59:35 +0100

    creation of banner component

---
+++ src/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+  return (
+    <header className='banner' style={{
+        backgroundsize:"cover",
+        backgroundImage:`url('https://webpages.scu.edu/ftp/lrlam/Images/Netflix_banner.jpg')`,
+        backgroundPosition:"center center",
+    }}>
+    <div className="banner__contents">
+        <h1 className='banner__title'>Movie Name</h1>
+        <div className="banner__buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner_description">This is a test description</h1>
+    </div>
+    <div className="banner--fadeBottom" />
+    </header>
+  )
+}
+
+export default Banner
