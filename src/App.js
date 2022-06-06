a1a30b24b0c15d6f8155e4b04d81f3d4ac3d5809
dansheddy25 <philipshedrack5@gmail.com>
2022-06-06 23:53:41 +0100

    signup screen added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Switch
 } from "react-router-dom";
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+  const user = null;
   return (
     <div className="app">
     <Router>
-    <Routes>
-        <Route path="/" >
-        <HomeScreen />
+    {!user ? (
+      <LoginScreen />
+    ) : (
+      <Routes>
+        <Route path="/" 
+          element = {<HomeScreen />} > 
         </Route>
       </Routes>
+    )
+
+    }
+      
     </Router>
     </div>
   );
