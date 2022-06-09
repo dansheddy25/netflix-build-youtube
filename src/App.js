b55c21a1d91564863785e130e0c07db1e124b6d8
dansheddy25 <philipshedrack5@gmail.com>
2022-06-09 01:47:50 +0100

    added some designs to profile.css

--- src/App.js
+++ src/App.js
@@ -32,12 +32,12 @@ function App() {
       }
       else{
         //logged out 
-        dispatch(logout)
+        dispatch(logout())
       }
     })
 
     return unsubscribe;
-  }, [])
+  }, [dispatch])
 
   return (
     <div className="app">
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-import { auth } from '../firebase';
+import { auth, logout } from '../firebase';
 import { selectUser } from '../app/features/userSlice';
 import Nav from "../Nav";
 import './ProfileScreen.css';
@@ -12,14 +12,14 @@ function ProfileScreen() {
     <Nav />
     <div className="profileScreen__body">
         <h1>Edit Profile</h1>
-        <div className="profileScreen__input">
+        <div className="profileScreen__info">
             <img src="https://pro2-bar-s3-cdn-cf.myportfolio.com/dddb0c1b4ab622854dd81280840458d3/8e65207aca8751179e10e03c_rw_600.png?h=506076a9f26fdf62293000f07c8c3c78"
              alt="" />
              <div className="profileScreen__details">
                 <h2> {user.email} </h2>
                 <div className="profileScreen__plans">
-
-                    <button onClick={() => auth.signOut}
+                    <h3>Plans</h3>
+                    <button onClick={() => auth.signOut()}
                      className='profileScreen__signOut'>Sign Out</button>
                 </div>
              </div>
